7c0f87dd43d9e43babde41ef7fca9c1f2e3aec09
Philippe Antoniotti <pantoniotti@gmail.com>
2015-01-10 21:28:40 -0500

    Added recipient to guest class and new seed file

--- lib/draw/guest.rb
+++ lib/draw/guest.rb
@@ -1,14 +1,11 @@
 class Guest
 
-  attr_accessor :name, :joint
+  attr_accessor :name, :joint, :recipient
 
-  def initialize(name = nil, joint = nil)
+  def initialize(name = nil, joint = nil, recipient = nil)
     @name = name
     @joint = joint
-  end
-
-  def print
-
+    @recipient = recipient
   end
 
 end
